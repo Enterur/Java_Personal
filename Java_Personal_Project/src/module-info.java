eac0abc1f6aaea259bf9ae6972b907b023c95fb4
Enterur <kimmin12327@gmail.com>
2025-09-15 16:12:52 +0900

    Github_Week2_commit_and_push

---
+++ Java_Personal_Project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Java_Personal_Project {
+}
---
+++ Java_Personal_Project/src/week02/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,14 @@
+package week02.ch02.sec01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		// 변수 선언 후 초기화 안 하면 컴파일 에러
+		int value = 5;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+
+}
